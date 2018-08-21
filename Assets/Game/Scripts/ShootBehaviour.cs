6ba15950dc8f76b0721e5cb8d118271ba5e1fd88
Akirame <gsvillalba12@hotmail.com>
2018-08-21 12:48:53 -0300

    Add Weapon & Bullet Behaviours

---
+++ Assets/Game/Scripts/ShootBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBehaviour : MonoBehaviour
+{
+    public bulletBehaviour bullet;
+    public GameObject shootPoint;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject go = Instantiate(bullet.gameObject, shootPoint.transform.position, Quaternion.identity);
+            go.GetComponent<Rigidbody>().velocity = transform.TransformDirection(0, 0, 25);
+            go.transform.rotation = shootPoint.transform.rotation;
+        }
+    }
+}
---
+++ Assets/Game/bulletBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletBehaviour : MonoBehaviour
+{
+    public float speed = 2;
+    private Rigidbody rig;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        rig.velocity = transform.forward * speed;
+    }
+}
