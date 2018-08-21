fa144462ee66eedeebafccaee8cbf76f7382dbc0
Unknown <dani_mrx93@hotmail.com>
2018-08-21 11:45:48 -0300

    Add MainMenu and Credits scenes

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour {
+
+    private string menuScene = "MainMenuScene";
+
+    public void OnBackPressed()
+    {
+        SceneManager.LoadScene(menuScene);
+    }
+
+}
