f57d1e22bcd3252c79ceb3d3bcd85f7f4183c64a
Unknown <dani_mrx93@hotmail.com>
2018-08-27 22:33:10 -0300

    Add Player script

--- Assets/Game/Scripts/Explosion.cs
+++ Assets/Game/Scripts/Explosion.cs
@@ -1,13 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Explosion : MonoBehaviour {
 
-    private float explosionRadius;
+    public float explosionRadius;
+    public float explosionAcceleration;
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update() {
+        Explode();
+    }
+
+    private void Explode()
+    {
+        float currentRadius = GetComponent<SphereCollider>().radius;
+        if (currentRadius < explosionRadius)
+        {
+            currentRadius += Time.deltaTime * explosionAcceleration;
+            GetComponent<SphereCollider>().radius = currentRadius;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Vector3 dir = other.transform.position - transform.position;
+            dir = dir.normalized;
+            other.gameObject.GetComponent<Player>().Knockback(dir);
+        }
+    }
 }
