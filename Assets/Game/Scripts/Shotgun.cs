020566c14bf282bf1979bf2f3debfb06fa2cfc96
Akirame <gsvillalba12@hotmail.com>
2018-09-07 18:56:40 -0300

    Add Shotgun

---
+++ Assets/Game/Scripts/Shotgun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : Gun
+{
+    public int pellets = 10;
+    public float inaccurracy = 0.5f;
+    public override void Shoot()
+    {
+        for (int i = 0; i < pellets; i++)
+        {
+            GameObject go = Instantiate(bullet.gameObject, shootPoint.transform.position, Quaternion.identity);
+            go.GetComponent<BulletBehaviour>().SetDirection((shootPoint.transform.forward + (Random.insideUnitSphere * inaccurracy)));
+        }
+    }
+}
