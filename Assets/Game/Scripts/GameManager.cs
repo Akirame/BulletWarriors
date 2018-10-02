08ea67ed6ea3c839af87e780228d77bebeb6af51
Unknown <dani_mrx93@hotmail.com>
2018-10-01 22:41:54 -0300

    Add Explosion sprite and GameManager

---
+++ Assets/Game/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Player player;
+
+    #region singleton
+    private static GameManager instance;
+    public static GameManager GetInstance()
+    {
+        return instance;
+    }
+    private void Awake()
+    {
+        if (!instance)
+            instance = this;
+        else
+            Destroy(this.gameObject);
+    }
+    #endregion
+
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
