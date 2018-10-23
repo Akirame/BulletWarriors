b54035c605c0da12e55216339e0f99323f7a9dfe
unknown <pehuen-testa@hotmail.com>
2018-10-23 12:00:00 -0300

    Add RigidbodyPlayer & Jump Platform

---
+++ Assets/JumpingPlatform.cs
@@ -0,0 +1,19 @@
+﻿    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class JumpingPlatform : MonoBehaviour
+    {
+
+        public float jumpForce = 500f;
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+            other.gameObject.GetComponent<Rigidbody>().AddForce(transform.up * jumpForce,ForceMode.Impulse);
+            
+            }
+        }    
+    }
